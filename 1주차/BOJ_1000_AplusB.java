4395e53d22c9a2e39692f66b43d583c5ffeeee9d
이은별 <leee24638@gmail.com>
2020-07-14 23:34:30 +0900

    solve BOJ_1000_AplusB

---
+++ 1주차/BOJ_1000_AplusB.java
@@ -0,0 +1,12 @@
+//1주차 day5_0713
+import java.util.Scanner;
+public class BOJ_1000_AplusB {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a+b);
+
+    }
+}
